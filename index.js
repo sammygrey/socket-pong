ec1d381a2b0689a30adb6c6d55ed60b1f2f2af95
guiisgooey <49354894+guiisgooey@users.noreply.github.com>
2021-10-16 21:38:58 -0400

    fixed some spacing issues with the display

--- index.js
+++ index.js
@@ -6,40 +6,40 @@ readline.emitKeypressEvents(process.stdin);
 
 if (process.stdin.isTTY) process.stdin.setRawMode(true);
 
-const width = 30; //display width its actually more like half of this
+const width = 60; //display width its actually more like half of this
 const height = 15; //display height
 let paddle, ball, opponent_paddle;
 let toDraw = [];
-let fps = 5;
+let fps = 7;
 let c;
 
 function draw() {
   console.clear();
-  console.log(" --------------------------------------------------------- ");
+  console.log(" ---------------------------------------------------------- ");
   console.log(
-    `  Player 1: ${paddle.score}                                 Player 2: ${opponent_paddle.score}`
+    `  Player 1: ${paddle.score}                                  Player 2: ${opponent_paddle.score}`
   );
-  console.log(" --------------------------------------------------------- ");
+  console.log(" ---------------------------------------------------------- ");
   for (let i = 0; i < height; i++) {
     let l = "";
     for (let i2 = 0; i2 < width; i2++) {
       if (isArrayItemExists(toDraw, [i2, i])) {
         if (i2 == ball.position[0] && i == ball.position[1]) {
-          l += "\x1b[37m ●";
+          l += "\x1b[37m●";
         } else {
-          l += "\x1b[37m |";
+          l += "\x1b[37m|";
         }
       } else {
-        l += "\x1b[30m ○";
+        l += "\x1b[30m○";
       }
     }
     console.log(l);
   }
   console.log(
-    "\x1b[37m --------------------------------------------------------- "
+    "\x1b[37m ---------------------------------------------------------- "
   );
-  console.log("  r to restart                           ctrl + c to quit ");
-  console.log(" --------------------------------------------------------- ");
+  console.log("  r to restart                            ctrl + c to quit ");
+  console.log(" ---------------------------------------------------------- ");
 }
 
 function update() {
@@ -55,14 +55,14 @@ function update() {
 
 function start() {
   places = [
-    [0, 10],
-    [0, 11],
-    [0, 12],
+    [1, 6],
+    [1, 7],
+    [1, 8],
   ];
   opponent_places = [
-    [28, 10],
-    [28, 11],
-    [28, 12],
+    [58, 6],
+    [58, 7],
+    [58, 8],
   ];
   paddle = new Paddle(places, "w", "s");
   ball = new Ball();
@@ -120,7 +120,7 @@ let direction_array = [1, -1];
 class Ball {
   constructor() {
     this.position = [
-      Math.floor(Math.random() * (23 - 7) + 7),
+      Math.floor(Math.random() * (40 - 20) + 20),
       Math.floor(Math.random() * (10 - 5) + 5),
     ]; //for math.random within two values *(max-min)+min
     this.direction = [
@@ -138,8 +138,8 @@ class Ball {
       this.direction[1] *= -1;
     }
     if (
-      this.position[0] + this.direction[0] >= 28 ||
-      this.position[0] + this.direction[0] <= 0
+      this.position[0] + this.direction[0] >= 58 ||
+      this.position[0] + this.direction[0] <= 1
     ) {
       //logic for scoring
       //logic for paddle bounce
@@ -154,10 +154,10 @@ class Ball {
         ])
       ) {
         this.direction[0] *= -1;
-        if (fps < 15) {
+        if (fps < 20) {
           fps++;
         }
-      } else if (this.position[0] + this.direction[0] >= 28) {
+      } else if (this.position[0] + this.direction[0] >= 58) {
         paddle.increase_score();
         this.reset();
       } else {
@@ -175,14 +175,14 @@ class Ball {
   reset() {
     //reset ball to another random position/direction
     this.position = [
-      Math.floor(Math.random() * (23 - 7) + 7),
+      Math.floor(Math.random() * (40 - 20) + 20),
       Math.floor(Math.random() * (10 - 5) + 5),
     ];
     this.direction = [
       direction_array[Math.floor(Math.random() * direction_array.length)],
       direction_array[Math.floor(Math.random() * direction_array.length)],
     ];
-    fps = 5;
+    fps = 7;
   }
 
   draw() {
